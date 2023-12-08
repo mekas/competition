40d9911e1eafdbdfe7354dedc970b8c838932b6a
Eka <eka.suryana@unj.ac.id>
2023-12-08 14:16:45 +0700

    push solution

--- p384.d
+++ p384.d
@@ -9,6 +9,29 @@ import std.string;
 void main(){
     string path = "../data";
     auto data = parse_input(path);
+    printArrayOfTuple(data);
+}
+
+void printArrayOfTuple(Array!(Tuple!(Array!bool, Array!bool)) data){
+    foreach (pair; data)
+    {
+        string bits1 = boolArrayToString(pair[0]);
+        string bits2 = boolArrayToString(pair[1]);
+        writefln("%s %s", bits1, bits2);
+    }
+}
+
+string boolArrayToString(Array!bool bits){
+    string bitstring = "";
+
+    foreach (bit; bits)
+    {
+        if(bit == true)
+            bitstring ~= "1";
+        else
+            bitstring ~= "0";
+    }
+    return bitstring;
 }
 
 Array!(Tuple!(Array!bool, Array!bool)) parse_input(string path){
@@ -23,6 +46,8 @@ Array!(Tuple!(Array!bool, Array!bool)) parse_input(string path){
         //writeln(strip(line));
         //split v to as individual character
         string[] letter = strip(line).split();
+        v = letter[0];
+        v2 = letter[1];
         Array!bool bletter = stringArrToBoolVector(v);
         Array!bool bletter2 = stringArrToBoolVector(v2);
         Tuple!(Array!bool, Array!bool) pair;
